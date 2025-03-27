14baa1512876336d15ef5d328eacd2b6b1f201f9
vidhishaprabhu <vidhishaprabhu@gmail.com>
2025-03-27 14:39:25 +0530

    Resolved

---
+++ resources/views/file-upload.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>File Upload</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>File Upload</h1>
+            </div>
+        </div>
+        <form action="">
+            <div class="row">
+                <div class="col-12">
+                    <input type="file" name="photo" accept=".jpg,.png,.jpeg">
+                </div>
+                <div class="col-12">
+                    <input type="submit" class="btn btn-sm btn-primary">
+                </div>
+            </div>
+        </form>
+    </div>
+    
+</body>
+</html>
